36e3d31e41b56438144bb0257e969e8ad7a7579d
vinibuffon-git <zagvinizag@gmail.com>
2022-11-22 20:07:48 -0300

    Conexao com o BD

--- conexao.php
+++ conexao.php
@@ -1,5 +1,15 @@
 <?php
 
-$conexcao = require('./banco.php');
+$conexao = require('./banco.php');
 
-//Pegar os dados que vieram no formulário e testar se são válidos
+$usuario = $_POST["usuario"];  
+
+$senha = $_POST["senha"];
+
+$sql = "SELECT * FROM usuarios WHERE login = :login AND senha = md5(md5(:senha))";
+
+$stmt = $conexao -> fetch(PDO::FETCH_ASSOC);
+$stmt -> bindValue(":login", $usuario);
+$stmt -> bindValue(":senha", $senha);
+
+$stmt
